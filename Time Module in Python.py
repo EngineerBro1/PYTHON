9bf4b19bd88b7ad39a776e5f8c01c48198bec406
EngineerBro1 <164660670+EngineerBro1@users.noreply.github.com>
2024-03-30 22:43:11 +0530

    Add files via upload

---
+++ Time Module in Python.py
@@ -0,0 +1,26 @@
+# The time module in Python provides various functions to work with time-related tasks, including retrieving the current time, sleeping, measuring time intervals, and formatting time values.
+
+# import time
+# currentTime = time.time()
+# print(currentTime)
+
+
+# import time
+# currentTime = time.time()
+# print(time.ctime(currentTime))
+
+
+# import time
+# currentTime = time.time()
+# print(time.gmtime(currentTime))
+
+
+# import time
+# currentTime = time.time()
+# print(time.localtime(currentTime))
+
+
+# import time
+# currentTime = time.localtime()
+# formattedTime = time.strftime("%Y-%m-%d %H:%M:%S",currentTime)
+# print(formattedTime)
